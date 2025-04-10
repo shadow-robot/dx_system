116c72eabcedeab7e6fc127f0c1d527c0332c178
niko-holmes <holmesnikolaus@gmail.com>
2025-04-10 15:34:06 +0000

    lint

--- host_scripts/commands.sh
+++ host_scripts/commands.sh
@@ -3,16 +3,18 @@
 # Software License Agreement (BSD License)
 # Copyright © 2024-2025 belongs to Shadow Robot Company Ltd.
 # All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
 #   1. Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
+#      this list of conditions and the following disclaimer.
 #   2. Redistributions in binary form must reproduce the above copyright notice,
 #      this list of conditions and the following disclaimer in the documentation
 #      and/or other materials provided with the distribution.
 #   3. Neither the name of Shadow Robot Company Ltd nor the names of its contributors
 #      may be used to endorse or promote products derived from this software without
 #      specific prior written permission.
+#
 # This software is provided by Shadow Robot Company Ltd "as is" and any express
 # or implied warranties, including, but not limited to, the implied warranties of
 # merchantability and fitness for a particular purpose are disclaimed. In no event
